d7ab537ddf28d7b830519655e318a598050184d9
JoshLong23 <41523136+JoshLong23@users.noreply.github.com>
2019-03-12 06:58:45 +0000

    config.yml repo updated created list of posts on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PostLink from '../components/post-link';
 
-export default () => {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges }
+  }
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
+
+  return <div>{Posts}</div>;
 };
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
